4fcfe2979fea88ce945ebe713a77955af466c3ac
ZouXinAn <209077335@qq.com>
2022-09-22 17:28:46 +0800

    chore: 详情页

--- src/view/listPage/cookDetail.vue
+++ src/view/listPage/cookDetail.vue
@@ -2,7 +2,7 @@
  * @Author: zoujiahao
  * @Date: 2022-09-22 15:10:17
  * @LastEditors: zoujiahao
- * @LastEditTime: 2022-09-22 16:14:53
+ * @LastEditTime: 2022-09-22 17:28:11
  * @FilePath: \CookBooks\src\view\listPage\cookDetail.vue
  * @Description: 
 -->
@@ -14,19 +14,43 @@
       </template>
     </van-nav-bar>
     <img class="bigPhoto" src="@/assets/image/cookDetailBigImg.png" alt="" />
+    <!-- 基础信息区域 -->
     <div class="cookInfo">
       <h2>麻婆豆腐</h2>
-      <div>
+      <div class="overviewDiv">
         <div>
-          <img src="@/assets/image/eyeIcon.png" />
+          <img src="@/assets/image/eyeIcon.png" style="height: 0.28rem; width: 0.4267rem" />
           <span> 20.4W </span>
         </div>
-        <div>
-          <img src="@/assets/image/smileIcon.png" />
+        <div style="margin-left: 0.6667rem">
+          <img src="@/assets/image/smileIcon.png" style="height: 0.32rem; width: 0.32rem" />
           <span> 18256 </span>
         </div>
       </div>
+      <div>
+        <img src="@/assets/image/splitLine.png" style="width: 1.2rem; height: 0.0933rem" alt="" />
+      </div>
+      <span class="authorSpan"> 作者: {{ '小甜心' }} </span>
+      <div class="cookSimpleInfo">
+        麻婆豆腐的特点是：在细嫩雪白的豆腐上，点缀着棕红色的牛肉酥馅，碧绿色的蒜苗，红红的辣油。视之如玉镶琥珀， 闻之则浓香扑鼻，食之更是集麻、辣、烫、嫩、酥、鲜、香 于一馔，让你越吃越上瘾
+      </div>
     </div>
+    <!-- 分割线 -->
+    <div class="splitLineDiv"></div>
+
+    <!-- 准备 -->
+    <div class="perpareDiv">
+      <span class="fontTitle">需要食材</span>
+      <div v-for="(item, i) in perpareList" :key="i">
+        <span>{{ item.name }}</span>
+        <span>{{ item.num }}{{ item.unit }}</span>
+      </div>
+    </div>
+    <div class="splitLineDiv" style="margin-top: 0"></div>
+    <div class="cookStepDiv">
+      <span class="fontTitle">烹饪步骤</span>
+    </div>
+
     <van-share-sheet v-model:show="isShowShare" title="立即分享给好友" :options="shareOptions" />
   </div>
 </template>
@@ -52,6 +76,60 @@ let shareOptions = $ref([
   ],
 ]);
 
+let perpareList = $ref([
+  {
+    name: '豆腐',
+    num: 1,
+    unit: '盒',
+  },
+  {
+    name: '肉末',
+    num: 150,
+    unit: 'g',
+  },
+  {
+    name: '蒜蓉酱',
+    num: 2,
+    unit: '瓣',
+  },
+  {
+    name: '玉米淀粉',
+    num: 1,
+    unit: '勺',
+  },
+  {
+    name: '蚝油',
+    num: 1,
+    unit: '勺',
+  },
+
+  {
+    name: '生抽',
+    num: 1,
+    unit: '勺',
+  },
+  {
+    name: '豆瓣酱',
+    num: 1,
+    unit: '大勺',
+  },
+  {
+    name: '姜末',
+    num: 150,
+    unit: 'g',
+  },
+  {
+    name: '花椒粒',
+    num: '',
+    unit: '适量',
+  },
+  {
+    name: '食用油',
+    num: '',
+    unit: '适量',
+  },
+]);
+
 let cookId = $route.query?.cookId ?? 1000;
 console.log(cookId);
 
@@ -65,7 +143,21 @@ const onClickRight = () => {
 </script>
 
 <style lang="scss" scoped>
+@import '@/style/common.scss';
 #cookDetail {
+  color: #242424;
+  .splitLineDiv {
+    width: 100vw;
+    height: 0.2933rem;
+    background: #efefef;
+    margin: 0.4rem 0;
+    margin-bottom: 0;
+  }
+  .fontTitle {
+    font-size: 0.48rem;
+    font-weight: 600;
+    color: #242424;
+  }
   .navBar {
     background: #feaa03;
     :deep(.van-nav-bar__title) {
@@ -85,11 +177,67 @@ const onClickRight = () => {
   .cookInfo {
     display: flex;
     flex-direction: column;
-    align-content: center;
+    // align-content: center;
+    text-align: center;
     h2 {
       font-size: 0.5333rem;
       color: #242424;
+      margin: 0.5067rem 0 0.2667rem;
     }
+    .overviewDiv {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-bottom: 0.5333rem;
+      img {
+        vertical-align: middle;
+      }
+      > div {
+        @include flexOne(center);
+        img {
+          margin-right: 0.0933rem;
+        }
+      }
+    }
+    .authorSpan {
+      margin-top: 0.2267rem;
+      font-size: 0.3733rem;
+      font-weight: 500;
+    }
+    .cookSimpleInfo {
+      font-size: 0.3467rem;
+      font-weight: 400;
+      margin-top: 0.4533rem;
+      padding: 0 0.6rem;
+      text-align: start;
+    }
+  }
+
+  .perpareDiv {
+    padding: 0.4rem 0.5333rem;
+    > div {
+      height: 1.2933rem;
+      width: calc(100% - 1.2rem);
+      display: flex;
+      align-items: center;
+      border-bottom: 2px solid #efefef;
+      padding-left: 0.6667rem;
+      span {
+        font-size: 0.4rem;
+        font-weight: 400;
+        color: #242424;
+      }
+      span:nth-child(1) {
+        display: inline-block;
+        width: 4.5333rem;
+      }
+    }
+    > div:nth-last-child(1) {
+      border-bottom: 0;
+    }
+  }
+  .cookStepDiv {
+    padding: 0.4rem 0.5333rem;
   }
 }
 </style>
